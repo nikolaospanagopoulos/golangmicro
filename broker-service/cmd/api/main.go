8d6970b11d4fb68bb42a748f0425136a837223d7
nikolaospanagopoulos <nikos4222@outlook.com.gr>
2023-05-09 02:25:30 +0300

    feat: initial

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "3000"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting broker on port: %s \n", webPort)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Panic(err)
+	}
+}
---
+++ front-end/go.mod
@@ -0,0 +1,3 @@
+module github.com/nikolaospanagopoulos/frontgo
+
+go 1.20
